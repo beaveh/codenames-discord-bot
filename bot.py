8c057d1af3601f1b41ce02f4dea5659ba5c0c613
Brandon Tran <43356317+beaveh@users.noreply.github.com>
2019-10-26 16:34:25 -0700

    Add cogs

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-import discord, random
+import discord, random, os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '$')
 
-@client.event
-async def on_ready():
-    print('Bot is ready.')
+# @client.event
+# async def on_ready():
+#     print('Bot is ready.')
 
 @client.event
 async def on_member_join(member):
@@ -15,14 +15,31 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has left a server')
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
+# @client.command()
+# async def ping(ctx):
+#     await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
 
 @client.command(aliases=['8ball', 'test'])
 async def _8ball(ctx, *, question): # *, question allows us to take several arguments as just 1
     responses = ['Yes', 'No', 'Uncertain']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 #temporary command to kill the bot
 @client.command()
