31b925329010af9751ba4df2a3e438aaa7b0fb3b
Brandon Tran <43356317+beaveh@users.noreply.github.com>
2019-12-29 22:54:45 -0500

    Add Game object

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
+class Game(object):
+    """Object that manages game state"""
+
+    def __init__(self):
+        self.players = {}
+
+    """Add player to a team"""
+    def add(self, player, team): #figure out what player is (discord id, string, etc.)
+        if player in self.players:
+            return False
+        else:
+            self.players[player] = team
+            return True
 
 class Word(object):
     """Represents a word on the board"""
